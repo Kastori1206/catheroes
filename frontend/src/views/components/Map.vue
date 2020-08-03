92e950e9955cffb5e5d814ecb7354bb7ec1fa66b
김현국 <khg6152450@naver.com>
2020-08-03 18:09:49 +0900

    Modify Cat Create - Location

--- frontend/src/views/components/Map.vue
+++ frontend/src/views/components/Map.vue
@@ -1,219 +1,359 @@
 <template>
   <div style="text-align: center;">
-    <br><br><br>
-    <div >
-    <vue-daum-map
+    <br />
+    <br />
+    <br />
+    <h2>{{centerDong}}</h2>
+    <div>
+      <vue-daum-map
+        :appKey="appKey"
+        :center.sync="center"
+        :level.sync="level"
+        :mapTypeId="mapTypeId"
+        :libraries="libraries"
+        @load="onLoad"
+        @center_changed="onMapEvent('center_changed', $event)"
+        @zoom_start="onMapEvent('zoom_start', $event)"
+        @zoom_changed="onMapEvent('zoom_changed', $event)"
+        @bounds_changed="onMapEvent('bounds_changed', $event)"
+        @click="onMapEvent('bounds_changed', $event)"
+        @dblclick="onMapEvent('dblclick', $event)"
+        @rightclick="onMapEvent('rightclick', $event)"
+        @mousemove="onMapEvent('mousemove', $event)"
+        @dragstart="onMapEvent('dragstart', $event)"
+        @drag="closeOverlay"
+        @dragend="onMapEvent('dragend', $event)"
+        @idle="onMapEvent('idle', $event)"
+        @tilesloaded="onMapEvent('tilesloaded', $event)"
+        @maptypeid_changed="onMapEvent('maptypeid_changed', $event)"
+        style="height: 60vh; margin: auto;"
+      ></vue-daum-map>
 
-      :appKey="appKey"
-
-      :center.sync="center"
-      :level.sync="level"
-      :mapTypeId="mapTypeId"
-      :libraries="libraries"
-
-      @load="onLoad"
-
-      @center_changed="onMapEvent('center_changed', $event)"
-      @zoom_start="onMapEvent('zoom_start', $event)"
-      @zoom_changed="onMapEvent('zoom_changed', $event)"
-      @bounds_changed="onMapEvent('bounds_changed', $event)"
-      @click="onMapEvent('bounds_changed', $event)"
-      @dblclick="onMapEvent('dblclick', $event)"
-      @rightclick="onMapEvent('rightclick', $event)"
-      @mousemove="onMapEvent('mousemove', $event)"
-      @dragstart="onMapEvent('dragstart', $event)"
-      @drag="closeOverlay"
-      @dragend="onMapEvent('dragend', $event)"
-      @idle="onMapEvent('idle', $event)"
-      @tilesloaded="onMapEvent('tilesloaded', $event)"
-      @maptypeid_changed="onMapEvent('maptypeid_changed', $event)"
-
-      style="height: 60vh; margin: auto;">
-    </vue-daum-map>
-
-    <h2>Options</h2>
-    <table>
-      <colgroup>
-        <col width="60" />
-        <col />
-      </colgroup>
-      <tr>
-        <th>레벨</th>
-        <td><input type="range" min="1" max="14" v-model.number="level"> {{level}}</td>
-      </tr>
-      <tr>
-        <th>경도</th>
-        <td><input type="number" v-model.number="center.lat" step="0.0001"></td>
-      </tr>
-      <tr>
-        <th>위도</th>
-        <td><input type="number" v-model.number="center.lng" step="0.0001"></td>
-      </tr>
-    </table> 
+      <table>
+        <colgroup>
+          <col width="60" />
+          <col />
+        </colgroup>
+        <tr>
+          <th>레벨</th>
+          <td>
+            <input type="range" min="1" max="14" v-model.number="level" />
+            {{level}}
+          </td>
+        </tr>
+        <tr>
+          <th>경도</th>
+          <td>
+            <input type="number" v-model.number="center.lat" step="0.0001" />
+          </td>
+        </tr>
+        <tr>
+          <th>위도</th>
+          <td>
+            <input type="number" v-model.number="center.lng" step="0.0001" />
+          </td>
+        </tr>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
 //   import '../../assets/css/style.scss'
-  import VueDaumMap from 'vue-daum-map';
-  import config from './config';
-  import axios from 'axios';
-  const SERVER_URL = "http://localhost:8080";
-  
-  export default {
-    name: 'App',
-    components: {VueDaumMap},
-   
-    data: () => ({
-      appKey: config.appKey,
-      center: {lat:36.1069968, lng:128.4163122},
-      level: 3,
-      mapTypeId: VueDaumMap.MapTypeId.NORMAL,
-      libraries: [],
-      mapObject: null,
-      myoverlay: [],
-      mymap: null,
-      mymarker: null,
-      Catinfo: {        
-        nickname: null,
-        age: null,        
-        breed : null,
-        location: null,
-        attr: null,
-        nickconditionsname: null,
-        image: null,
-        lat: null,
-        lng: null,
-        catid : null,
-      }
-    }),
-    props: {
-      iscreate: Boolean,
+import VueDaumMap from "vue-daum-map";
+import config from "./config";
+import axios from "axios";
+// import geocoder from "Geocoder";
+
+const SERVER_URL = "http://localhost:8080";
+
+export default {
+  name: "App",
+  components: { VueDaumMap },
+
+  data: () => ({
+    appKey: config.appKey,
+    center: { lat: 36.1069968, lng: 128.4163122 },
+    level: 3,
+    mapTypeId: VueDaumMap.MapTypeId.NORMAL,
+    libraries: [],
+    mapObject: null,
+    myoverlay: [],
+    mymap: null,
+    mymarker: null,
+    geocoder: null,
+    centerDong: null,
+    Catinfo: {
+      nickname: null,
+      age: null,
+      breed: null,
+      location: null,
+      attr: null,
+      nickconditionsname: null,
+      image: null,
+      lat: null,
+      lng: null,
+      catid: null
+    }
+  }),
+  props: {
+    iscreate: Boolean
+  },
+
+  methods: {
+    senddata() {
+      this.$emit("send-data", this.center);
     },
+    onLoad(map) {
+      ///////// 현재위치 받아오기/////////
+      if (navigator.geolocation) {
+        // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+        navigator.geolocation.getCurrentPosition(position => {
+          this.center.lat = position.coords.latitude;
+          this.center.lng = position.coords.longitude;
+        });
+      }
 
-    methods: {
-        senddata() {
-          this.$emit('send-data', this.center)
-        },
-        onLoad (map) {
-        console.log(map)
-        ////////////////
-        // this.Catinfo.nickname = "삼색이"
-        // this.Catinfo.breed = "잡종"
-        // this.Catinfo.catid = 1
-        ////////////////
-        this.mymap = map
-        // 지도의 현재 영역을 얻어옵니다
-        var bounds = map.getBounds();
-        // 영역정보를 문자열로 얻어옵니다. ((남,서), (북,동)) 형식입니다
-        var boundsStr = bounds.toString();
-        console.log('Daum Map Loaded', boundsStr);
-        this.mapObject = map;   
-        
-        this.loadMarker();
-      },
-      loadMarker(){
-        console.log('고양이정보받아오라고했다')
-        axios
-          .post(SERVER_URL + "/cat/catList")
-          .then(res => {
-            res.data.forEach(e => {
-              console.log(e);
-              // this.catinfo.nickname = e.nickName;
-              // this.catinfo.age = e.age;
-              // this.catinfo.breed = e.breed;
-              // this.catinfo.location = e.location;
-              // this.catinfo.character = e.character;
-              // this.catinfo.conditions = e.conditions;
-              // this.catinfo.image = 'https://raw.githubusercontent.com/khg6152450/AboutMe/master/CatMarker.png';
-              // this.catinfo.lat = e.lat;
-              // this.catinfo.lng = e.lng;
-              // this.catinfo.catid = e.catid;
+      //////////////////////////////////
 
-             var imageSrc = 'https://raw.githubusercontent.com/khg6152450/AboutMe/master/CatMarker.png', // 마커이미지의 주소입니다 
-             imageSize = new kakao.maps.Size(50, 50), // 마커이미지의 크기입니다
-             imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-      
-              // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-              var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+      /////좌표값에서 법정동 가져오기/////
+
+      // 주소-좌표 변환 객체를 생성합니다
+      this.geocoder = new kakao.maps.services.Geocoder();
+
+      // // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
+      this.searchAddrFromCoords(map.getCenter(), this.displayCenterInfo);
+
+      kakao.maps.event.addListener(map, "idle", () => {
+        this.searchAddrFromCoords(map.getCenter(),this. displayCenterInfo);
+      });
+      //////////////////////////////////
+
+      this.mymap = map;
+      // 지도의 현재 영역을 얻어옵니다
+      var bounds = map.getBounds();
+      // 영역정보를 문자열로 얻어옵니다. ((남,서), (북,동)) 형식입니다
+      var boundsStr = bounds.toString();
+
+      this.mapObject = map;
+
+      this.loadMarker();
+    },
+    loadMarker() {
+      // console.log('고양이정보받아오라고했다')
+      axios
+        .post(SERVER_URL + "/cat/catList")
+        .then(res => {
+          res.data.forEach(e => {
+            // console.log(e);
+            // this.catinfo.nickname = e.nickName;
+            // this.catinfo.age = e.age;
+            // this.catinfo.breed = e.breed;
+            // this.catinfo.location = e.location;
+            // this.catinfo.character = e.character;
+            // this.catinfo.conditions = e.conditions;
+            // this.catinfo.image = 'https://raw.githubusercontent.com/khg6152450/AboutMe/master/CatMarker.png';
+            // this.catinfo.lat = e.lat;
+            // this.catinfo.lng = e.lng;
+            // this.catinfo.catid = e.catid;
+
+            var imageSrc =
+                "https://raw.githubusercontent.com/khg6152450/AboutMe/master/CatMarker.png", // 마커이미지의 주소입니다
+              imageSize = new kakao.maps.Size(50, 50), // 마커이미지의 크기입니다
+              imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+            // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+            var markerImage = new kakao.maps.MarkerImage(
+                imageSrc,
+                imageSize,
+                imageOption
+              ),
               markerPosition = new kakao.maps.LatLng(e.lat, e.lng); // 마커가 표시될 위치입니다
 
-              // 마커를 생성합니다
-              let marker = new kakao.maps.Marker({
-                position: markerPosition, 
-                image: markerImage, // 마커이미지 설정             
-              });
+            // 마커를 생성합니다
+            let marker = new kakao.maps.Marker({
+              position: markerPosition,
+              image: markerImage // 마커이미지 설정
+            });
 
-              this.mymarker = marker;
+            this.mymarker = marker;
 
-              // 마커가 지도 위에 표시되도록 설정합니다
-              marker.setMap(this.mymap);
-              
-              kakao.maps.event.addListener(marker, 'click', () => {
-                this.createOverlay(e);
-              });
-            })
-          })
-          .catch(error => {
-            console.log(error);
+            // 마커가 지도 위에 표시되도록 설정합니다
+            marker.setMap(this.mymap);
+
+            kakao.maps.event.addListener(marker, "click", () => {
+              this.createOverlay(e);
+            });
           });
-      },
-      createOverlay(data){
-          var content = 
-            '<div class="wrap">' + 
-            '    <div class="info2">' + 
-            '        <div class="title2">' + 
-                        '길냥이 정보' +                         
-            '        </div>' + 
-            '        <div class="body2">' + 
-            '            <div class="img">' +
-            '                <img src="https://raw.githubusercontent.com/khg6152450/AboutMe/master/CatMarker.png" width="73" height="70">' +
-            '           </div>' + 
-            '            <div class="desc">' + 
-            '                <div class="ellipsis">' + data.nickName + '</div>' + 
-            '                <div class="jibun ellipsis">' + data.breed + '</div>' + 
-            '                <div><a href="/detail/'+ data.catid + '" class="link">상세정보</a></div>' + 
-            '            </div>' + 
-            '        </div>' + 
-            '    </div>' + 
-            '</div>';                 
-          var overlay = new kakao.maps.CustomOverlay({
-            content: content,
-              map: this.mymap,
-              position: new kakao.maps.LatLng(data.lat, data.lng)
+        })
+        .catch(error => {
+          console.log(error);
         });
-        // this.myoverlay.setMap(null);
-        // console.dir(this.myoverlay);
-        this.myoverlay.push(overlay);
-      },
-      closeOverlay() {
-          // console.dir(this.myoverlay);
-          this.myoverlay.forEach(element => {
-            element.setMap(null);
-          });
-          // this.myoverlay.setMap(null);
-      },
-      onMapEvent (event, params) {
-        // console.log(`Daum Map Event(${event})`, params);
+    },
+    createOverlay(data) {
+      var content =
+        '<div class="wrap">' +
+        '    <div class="info2">' +
+        '        <div class="title2">' +
+        "길냥이 정보" +
+        "        </div>" +
+        '        <div class="body2">' +
+        '            <div class="img">' +
+        '                <img src="https://raw.githubusercontent.com/khg6152450/AboutMe/master/CatMarker.png" width="73" height="70">' +
+        "           </div>" +
+        '            <div class="desc">' +
+        '                <div class="ellipsis">' +
+        data.nickName +
+        "</div>" +
+        '                <div class="jibun ellipsis">' +
+        data.breed +
+        "</div>" +
+        '                <div><a href="/detail/' +
+        data.catid +
+        '" class="link">상세정보</a></div>' +
+        "            </div>" +
+        "        </div>" +
+        "    </div>" +
+        "</div>";
+      var overlay = new kakao.maps.CustomOverlay({
+        content: content,
+        map: this.mymap,
+        position: new kakao.maps.LatLng(data.lat, data.lng)
+      });
+      // this.myoverlay.setMap(null);
+      // console.dir(this.myoverlay);
+      this.myoverlay.push(overlay);
+    },
+    closeOverlay() {
+      // console.dir(this.myoverlay);
+      this.myoverlay.forEach(element => {
+        element.setMap(null);
+      });
+      // this.myoverlay.setMap(null);
+    },
+    onMapEvent(event, params) {
+      // console.log(`Daum Map Event(${event})`, params);
+    },
+    ////////
+    searchAddrFromCoords(coords, callback) {
+      // 좌표로 행정동 주소 정보를 요청합니다
+      this.geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
+    },
+    searchDetailAddrFromCoords(coords, callback) {
+      // 좌표로 법정동 상세 주소 정보를 요청합니다
+      this.sgeocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+    },
+    displayCenterInfo(result, status) {
+      if (status === kakao.maps.services.Status.OK) {
+        var infoDiv = document.getElementById("centerAddr");
+
+        for (var i = 0; i < result.length; i++) {
+          // 행정동의 region_type 값은 'H' 이므로
+          if (result[i].region_type === "H") {
+            console.log(result[i].address_name);
+            this.centerDong = result[i].address_name;
+            // infoDiv.innerHTML = result[i].address_name;
+            break;
+          }
+        }
       }
     }
+    //////////
   }
+};
 </script>
 
 <style>
-    .wrap {position: absolute;left: 0;bottom: 40px;width: 288px;height: 132px;margin-left: -144px;text-align: left;overflow: hidden;font-size: 12px;font-family: 'Malgun Gothic', dotum, '돋움', sans-serif;line-height: 1.5;}
-    .wrap * {padding: 0;margin: 0;}
-    .wrap .info2 {width: 286px;height: 120px;border-radius: 5px;border-bottom: 2px solid #ccc;border-right: 1px solid #ccc;overflow: hidden;background: #fff;}
-    .wrap .info2:nth-child(1) {border: 0;box-shadow: 0px 1px 2px #888;}
-    .info2 .close {position: absolute;top: 7.5px;right: 10px;color: #888;width: 17px;height: 17px;background: url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/overlay_close.png');}
-    .info2 .close:hover {cursor: pointer;}
-    .info2 .title2 {padding: 5px 0 0 10px;height: 30px;background: #eee;border-bottom: 1px solid #ddd;font-size: 18px;font-weight: bold;}    
-    .info2 .body2 {position: relative;overflow: hidden;}
-    .info2 .desc {position: relative;margin: 13px 0 0 90px;height: 75px;}
-    .desc .ellipsis {overflow: hidden;text-overflow: ellipsis;white-space: nowrap;}
-    .desc .jibun {font-size: 11px;color: #888;margin-top: -2px;}
-    .info2 .img {position: absolute;top: 6px;left: 5px;width: 73px;height: 71px;border: 1px solid #ddd;color: #888;overflow: hidden;}
-    .info2:after {content: '';position: absolute;margin-left: -12px;left: 50%;bottom: 0;width: 22px;height: 12px;background: url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png')}
-    .info2 .link {color: #5085BB;}
+.wrap {
+  position: absolute;
+  left: 0;
+  bottom: 40px;
+  width: 288px;
+  height: 132px;
+  margin-left: -144px;
+  text-align: left;
+  overflow: hidden;
+  font-size: 12px;
+  font-family: "Malgun Gothic", dotum, "돋움", sans-serif;
+  line-height: 1.5;
+}
+.wrap * {
+  padding: 0;
+  margin: 0;
+}
+.wrap .info2 {
+  width: 286px;
+  height: 120px;
+  border-radius: 5px;
+  border-bottom: 2px solid #ccc;
+  border-right: 1px solid #ccc;
+  overflow: hidden;
+  background: #fff;
+}
+.wrap .info2:nth-child(1) {
+  border: 0;
+  box-shadow: 0px 1px 2px #888;
+}
+.info2 .close {
+  position: absolute;
+  top: 7.5px;
+  right: 10px;
+  color: #888;
+  width: 17px;
+  height: 17px;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/overlay_close.png");
+}
+.info2 .close:hover {
+  cursor: pointer;
+}
+.info2 .title2 {
+  padding: 5px 0 0 10px;
+  height: 30px;
+  background: #eee;
+  border-bottom: 1px solid #ddd;
+  font-size: 18px;
+  font-weight: bold;
+}
+.info2 .body2 {
+  position: relative;
+  overflow: hidden;
+}
+.info2 .desc {
+  position: relative;
+  margin: 13px 0 0 90px;
+  height: 75px;
+}
+.desc .ellipsis {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.desc .jibun {
+  font-size: 11px;
+  color: #888;
+  margin-top: -2px;
+}
+.info2 .img {
+  position: absolute;
+  top: 6px;
+  left: 5px;
+  width: 73px;
+  height: 71px;
+  border: 1px solid #ddd;
+  color: #888;
+  overflow: hidden;
+}
+.info2:after {
+  content: "";
+  position: absolute;
+  margin-left: -12px;
+  left: 50%;
+  bottom: 0;
+  width: 22px;
+  height: 12px;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png");
+}
+.info2 .link {
+  color: #5085bb;
+}
 </style>
